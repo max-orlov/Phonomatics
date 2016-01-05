e46b1ae2a267025c8867799e636ad40939082eee
maayan <merlich1@gmail.com>
2016-01-05 16:37:38 +0200

    Using environment vars

--- tester.py
+++ tester.py
@@ -1,9 +1,9 @@
-from os import path
+from os import path, environ
 from json import dumps
 from PraatPlugin import VoiceAnalyzer
 
 v = VoiceAnalyzer()
-l = v.analyze(path.abspath('praat-prosody_v0.1.1\demo\samples\sample.wav'))
+l = v.analyze(path.abspath(environ['SAMPLES_PATH']))
 
 print dumps(l, indent=2)
 
